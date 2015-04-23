788acba32ef32afe6245a3bc59814de21b4090c9
Michael Gesche <mgesche@gmail.com>
2015-04-23 10:03:28 +0200

    Correction de la syntaxe

--- Ressources/SQL Server/Utils/TableToString.sql
+++ Ressources/SQL Server/Utils/TableToString.sql
@@ -2,8 +2,8 @@ CREATE FUNCTION [Utils].[TableToString](
 	@StrSchema		VARCHAR(50), 
 	@StrTable		VARCHAR(50), 
 	@StrChamp		VARCHAR(50),
-	@StrWhere		VARCHAR(500) = '',
-) RETURNS @StrResultat VARCHAR(MAX)
+	@StrWhere		VARCHAR(500) = ''
+) RETURNS VARCHAR(MAX)
 as
 BEGIN
 
@@ -16,15 +16,16 @@ N'@Resultat VARCHAR(MAX) OUTPUT'
 
 SET @Query = 
 N' SELECT  @Resultat = STUFF(('
-+' 			SELECT  '; ' + CAST('+@StrChamp+' AS VARCHAR(MAX))'
++' 			SELECT  ''; '' + CAST('+@StrChamp+' AS VARCHAR(MAX))'
 +'          FROM '+@StrSchema+'.'+@StrTable
-+'          FOR XML PATH(''), TYPE'
++'          FOR XML PATH(''''), TYPE'
 +' 			'+@StrWhere
 +'          ).value(''.'',''varchar(max)''),1,2, '''') '
 
 EXECUTE sp_executesql @Query, @ParamDefinition, @Resultat = @StrResultat OUTPUT
 
-RETURN
+RETURN @StrResultat
+
 END
 
 
